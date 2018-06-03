27b202f6d849c6f6af49989eb8f690dd5566d1db
weik55 <weik5@uw.edu>
2018-06-02 22:42:01 -0700

    Homepage structure and content
    - Added structure and styles to homepage
    - Added ability to fetch data from external movie db
    - Updated homepage views to display data
    - Added warnings for old unsupported browsers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,33 @@
+# A class that controlls the inputs and outputs of the homepage (Home)
+# To the reviewer: I could have installed a MovieDB wrapper as a gem to speed up production
+# of this app. However, I decided to instead to use the Net::HTTP calls just to show that I can.
+
+#TODO: Add stars
+#TODO: Make a "Movie" Class Object.
+#TODO: Test Ping Database to see if it works on startup
+#TODO: Get configuration and genre look-ups and store them on startup
+#TODO: Create individual pages for each movie.
+#TODO: Make a search
 class HomeController < ApplicationController
-  def index
-  end
+	require "net/http"
+	require "uri"
+
+	def index
+		#uri1 = URI.parse("https://api.themoviedb.org/3/configuration?api_key=fe6f1f7f211065dc50201f2985280755")
+		#response1 = Net::HTTP.get_response(uri1)
+		puts "Requesting movies from external database..."
+		#uri = URI.parse("https://api.themoviedb.org/3/movie/550?api_key=fe6f1f7f211065dc50201f2985280755")
+		uri = URI.parse("https://api.themoviedb.org/3/discover/movie?api_key=fe6f1f7f211065dc50201f2985280755&primary_release_date.gte=2018-04-01&primary_release_date.lte=2018-06-01")
+		response = Net::HTTP.get_response(uri)
+		@config = "http://image.tmdb.org/t/p/w154/"
+
+		response_body = JSON.parse(response.body)
+		@movies = response_body["results"]
+		@movies.each do |movie|
+			parsedDate = Date.parse(movie["release_date"])
+			movie["date"] = parsedDate.to_formatted_s(:long)
+		end
+  		#@config_as_text = response1.body
+  		@movie_as_text = response.body
+	end
 end
